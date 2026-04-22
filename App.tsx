ad4c9dbd0933e3632951172a495b1a3fe2dd5489
acaloracan <albert.caloracan@gmail.com>
2026-04-23 02:10:43 +0800

    Update app entry

--- App.tsx
+++ App.tsx
@@ -1,44 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar, StyleSheet } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import {
+  initialWindowMetrics,
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { AuthProvider } from './src/context/AuthContext';
+import { MainStackNavigator } from './src/navigation/MainNavigation';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <PaperProvider>
+        <AuthProvider>
+          <StatusBar barStyle={'dark-content'} />
+          <NavigationContainer>
+            <MainStackNavigator />
+          </NavigationContainer>
+        </AuthProvider>
+      </PaperProvider>
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
